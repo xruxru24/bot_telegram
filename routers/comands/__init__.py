a9a79fc6e311f84aa8c69b8f4f4dbd7974bd17cf
xruxru24 <ksarnenkov@gmail.com>
2024-05-19 01:13:55 +0300

    beta 2.0

--- routers/comands/__init__.py
+++ routers/comands/__init__.py
@@ -1,9 +1,10 @@
 from aiogram import Router
 from .quadratic import router as quadratic_equations_router
 from .arithmetic import router as arithmetic_router
-
+from .functions import router as functions_router
 
 router = Router()
 
 router.include_router(quadratic_equations_router)
 router.include_router(arithmetic_router)
+router.include_router(functions_router)
--- routers/comands/arithmetic.py
+++ routers/comands/arithmetic.py
@@ -6,25 +6,25 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 router = Router()
 
-help_arithmetic = 'help_quadratic_equations'
+help_arithmetic = 'help_arithmetic'
 
 
 @router.message(Command("arithmetic"))
 async def cmd_start(message: types.Message):
     await message.answer('arithmetic')
-    builder_start_quadratic = InlineKeyboardBuilder()
-    builder_start_quadratic.add(types.InlineKeyboardButton(
+    builder_start_arithmetic = InlineKeyboardBuilder()
+    builder_start_arithmetic.add(types.InlineKeyboardButton(
         text="подсказка",
         callback_data=help_arithmetic)
     )
     await message.answer(
         "напишите арфмитеческое действие",
-        reply_markup=builder_start_quadratic.as_markup()
+        reply_markup=builder_start_arithmetic.as_markup()
     )
 
 
 @router.callback_query(F.data == help_arithmetic)
-async def angly_callback_data_q_eq(callback_query: CallbackQuery):
+async def angly_callback_data_arithmetic(callback_query: CallbackQuery):
     await callback_query.answer(
         text='''
 1) бот умеет сравнивать примеры
